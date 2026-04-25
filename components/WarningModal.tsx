c0e83591dac2023ede3887bfc86a2a6376a4b459
Daksh Vasani <dakshvasani5@gmail.com>
2026-04-26 02:42:11 +0530

    Update WarningModal.tsx

--- components/WarningModal.tsx
+++ components/WarningModal.tsx
@@ -3,13 +3,13 @@
 export default function WarningModal({ onAgree }: { onAgree: () => void }) {
   return (
     <div className="fixed inset-0 bg-black/60 backdrop-blur-md z-[998] flex items-center justify-center">
-      <div className="bg-white p-8 rounded-3xl max-w-md text-center border-2 border-black">
+      <div className="   p-8 rounded-3xl max-w-md text-center border-2 bg-[#0a0a0a] border-white/10">
         
         <h2 className="text-xl font-black mb-4 uppercase">
           Warning
         </h2>
 
-        <p className="text-sm text-gray-600 mb-6">
+        <p className="text-sm text-white/60 mb-6">
           This is a fully anonymous platform.  
           Do not share personal, sensitive, or harmful content.  
           You are responsible for what you post.  
